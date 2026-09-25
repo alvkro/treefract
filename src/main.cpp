bdc7f65bebfc66ac4dbac9a76e321ee986361eb9
Alvaro Barbosa Filho <alvaro.filho.111@ufrn.edu.br>
2026-09-25 18:09:32 -0300

    add: implementação das funções turtle state na main

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,25 @@
 #include "../include/lsystem.hpp"
 #include "..//include/runningopt.hpp"
+#include "../include/turtlestate.hpp"
+#include <stack>
 #include <iostream>
 
-
-/// TODO: receber argumentos CLI para valores em generate
+/// TODO: receber um arquivo com os axiomas
 int main(int argc, char* argv[]) {
 	std::string output;
 	LSystem lsystem;
 	RunningOptions run_opt;
+	TurtleState turtlestate;
+	std::stack<TurtleState> state_stack;
 
 	run_opt.validate_arguments(argc, argv);
 	lsystem.setAxiom("X");
 	lsystem.addRule('F', "FF");
 	lsystem.addRule('X', "F[+X]");
 	output = lsystem.generate(run_opt.n_iterations);
-	std::cout << output << "\n";
+	
+	for (auto i : output) {
+		turtlestate.draw(i, state_stack);
+		std::cout << turtlestate.getPosition().x << ", " << turtlestate.getPosition().y << "\n";
+	}
 }
