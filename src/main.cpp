380c4463b482bd36098c52dc002ab8cfc81ed712
Alvaro Barbosa Filho <alvaro.filho.111@ufrn.edu.br>
2026-09-25 19:07:49 -0300

    new: salva em formato svg

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include "../include/lsystem.hpp"
 #include "..//include/runningopt.hpp"
 #include "../include/turtlestate.hpp"
-#include <stack>
+#include "../include/svg_exporter.hpp"
 #include <iostream>
+#include <stack>
+#include <vector>
 
 /// TODO: receber um arquivo com os axiomas
 int main(int argc, char* argv[]) {
@@ -11,15 +13,21 @@ int main(int argc, char* argv[]) {
 	RunningOptions run_opt;
 	TurtleState turtlestate;
 	std::stack<TurtleState> state_stack;
+	std::vector<LineSegment> lines;
 
 	run_opt.validate_arguments(argc, argv);
 	lsystem.setAxiom("X");
 	lsystem.addRule('F', "FF");
-	lsystem.addRule('X', "F[+X]");
+	lsystem.addRule('X', "F-[[X]+X]+F[+FX]-X");
 	output = lsystem.generate(run_opt.n_iterations);
 	
 	for (auto i : output) {
 		turtlestate.draw(i, state_stack);
-		std::cout << turtlestate.getPosition().x << ", " << turtlestate.getPosition().y << "\n";
+		if (i == 'F') {
+			lines.push_back({turtlestate.getStartPos(), turtlestate.getEndPos()});
+		}
+	}
+	if (SVGExporter::save(lines, "arvores.svg")) {
+		std::cout << "Imagem salva" << "\n";
 	}
 }
