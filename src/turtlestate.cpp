ded558f61bca0b41443af92aa0673a12d2125c1a
Alvaro Barbosa Filho <alvaro.filho.111@ufrn.edu.br>
2026-09-25 18:11:50 -0300

    new: draw()

--- src/turtlestate.cpp
+++ src/turtlestate.cpp
@@ -2,11 +2,9 @@
 #include <stack>
 #include "../include/turtlestate.hpp"
 
-/// TODO: Resolver a questão da conversão de graus para radianos.
-
 double TurtleState::deg_to_rad(double this_angle) { return this_angle * (pi / 180); }
 
-void TurtleState::update_position(const double& angle) { this->position = {position.x + l * std::cos(angle), position.y + l * std::sin(angle)}; }
+void TurtleState::update_position() { this->position = {position.x + l * std::cos(this->angle), position.y + l * std::sin(this->angle)}; }
 
 void TurtleState::rotate(const char side) {
 	if (side == '+') {
@@ -16,9 +14,19 @@ void TurtleState::rotate(const char side) {
 	}
 }
 
-void TurtleState::create_branch(char savepoint) {
-	//* A ideia é percorrer toda a string, se achar [
-	// a função começa e só termina se achar ].
-	std::stack<char> branch;
+void TurtleState::draw(char c, std::stack<TurtleState>& state_stack) {
+	if (c == 'F') {
+		start_pos = this->position;
+		update_position();
+		end_pos = this->position;
+	} else if (c == '+' || c == '-') {
+		rotate(c);
+	} else if (c == '[') {
+		state_stack.push(*this);
+	} else if(c == ']') {
+		*this = state_stack.top();
+		state_stack.pop();
+	}
+
 }
 
